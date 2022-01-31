50a9bcd0b47a2158c0680fb2d261cb25780f6f0d
ipskiff <ipskiff007@gmail.com>
2022-01-31 02:15:47 +0200

    4. MVC HttpServletRequest @RequestParam

--- src/main/java/ru/satizhanov/myspringapp/controllers/FirstController.java
+++ src/main/java/ru/satizhanov/myspringapp/controllers/FirstController.java
@@ -3,13 +3,21 @@ package ru.satizhanov.myspringapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-//@RequestMapping("/first")
+@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    //public String helloPage(HttpServletRequest request) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
